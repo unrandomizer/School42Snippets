76df8c364f2d6baee08628a3e34baabb47a5286d
Hannity Vayne <therostikru@gmail.com>
2020-02-25 02:36:25 +0300

    moved to lib folder

---
+++ ft_lib/ft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hvayne <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/15 23:09:27 by hvayne            #+#    #+#             */
+/*   Updated: 2020/02/15 23:20:09 by hvayne           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_swap(int *a, int *b);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+char	*ft_strcpy(char *dest, char *src);
+
+#endif
