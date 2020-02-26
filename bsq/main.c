071ff7d3a31458658706e67145fcfbd15d9143ac
Hannity Vayne <therostikru@gmail.com>
2020-02-26 04:32:07 +0300

    just sync

--- bsq/main.c
+++ bsq/main.c
@@ -6,14 +6,13 @@
 /*   By: hvayne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 00:44:35 by hvayne            #+#    #+#             */
-/*   Updated: 2020/02/25 08:23:11 by hvayne           ###   ########.fr       */
+/*   Updated: 2020/02/26 04:31:46 by hvayne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
-
+#include <unistd.h>
 void putnbr(int nbr)
 {
 	char c;
@@ -30,48 +29,89 @@ void putnbr(int nbr)
 	}
 }
 
-void putstr(char *str)
+void putstr(char* str)
 {
-	while(*str)
+	while (*str)
 	{
 		write(1, str++, 1);
 	}
 }
 
-int		strlen_to_n(char *str)
+int		strlen_to_n(char* str)
 {
 	int len;
 
 	len = 0;
-	while(*str != '\n' || *str != '\0')
+	while (*str != '\n' || *str != '\0')
 	{
 		len++;
 		str++;
-	}	
+	}
 	return len;
 }
 
-void	read_bsq(int fd, int buff_size)
+char** read_bsq(int fd, const int buff_size)
 {
-	char str[buff_size];
+	char** map;
+	char* str = malloc(sizeof(char) * 21042);
 	int count;
+
 	int lines_count;
 	int i;
+	int j;
 
 	i = 0;
+	j = 0;
+	lines_count = 0;
 	count = read(fd, str, buff_size);
-	while (str[i] >= '0' && str[i] <= 9)
+	
+	if (count <= 0)
 	{
-		lines_count = lines_count * 10 + str[i] - 48;
+		return (NULL);
+	}
+	while(*str >= '0' && *str <= '9')
+	{
+		lines_count = lines_count * 10 + *str;
+		str++;
 	}
 	putnbr(lines_count);
+	return (NULL);
+	/*
+	while (42)
+	{
+		while (21)
+		{
+			if (*str == '\n')
+				break;
+			if (*str == '\0')
+				break;
+			str++;
+		}
+		str++;
+		while (i < 8)
+		{
+			while (j < 12)
+			{
+				if (!(*str == '.' || *str == 'o'))
+					return (NULL);
+				map[i][j] = *str;
+				str++;
+				j++;
+			}
+			str++;
+			j = 0;
+			i++;
+		}
+	}
+	free(str);*/
 }
 
 int		main(void)
 {
 	int fd;
-	fd = open("square.map", O_RDONLY);
-	read_bsq(fd, 8192);
-	close (fd);
+	fd = open("12x08.map", O_RDONLY);
+	const int buffsize = 8192;
+	char** map = read_bsq(fd, buffsize);
+	close(fd);
 	return (0);
 }
